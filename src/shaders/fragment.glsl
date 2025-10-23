8a6e5317f38792d0acd20e0e0d72df01e5ee2ee4
Rasmus Steen <rasmus@steen.ninja>
2025-10-23 20:23:14 +0200

    made particles more vibrant and bubble like

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -2,10 +2,12 @@ varying vec3 vColor;
 
 void main() {
     float dist = distance(gl_PointCoord, vec2(0.5));
-    float strength = 1.0 - smoothstep(0.3, 0.5, dist);
+    float strength = 1.0 - smoothstep(0.45, 0.5, dist);
+    float multiplied = dist * 1.5;
+    float edge = multiplied * multiplied;
 
     vec3 centerColor = vec3(1.0, 1.0, 1.0);
-    vec3 finalColor = mix(centerColor, vColor, dist);
+    vec3 finalColor = mix(vColor, centerColor, edge);
 
     gl_FragColor = vec4(finalColor, strength);
 }

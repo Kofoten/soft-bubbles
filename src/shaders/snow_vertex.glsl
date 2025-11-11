3113fd5e2857a469dabbca96e2759d81afb0295e
Rasmus Steen <rasmus@steen.ninja>
2025-11-11 16:09:15 +0100

    adding 'snow' shape

---
+++ src/shaders/snow_vertex.glsl
@@ -0,0 +1,12 @@
+attribute float size;
+attribute vec3 color;
+
+varying vec3 vColor;
+
+void main() {
+    vColor = color;
+
+    vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+    gl_Position = projectionMatrix * mvPosition;
+    gl_PointSize = size * (-mvPosition.z);
+}
